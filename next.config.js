b672b7efc0e4502c99d415ccf70775001afce1e2
indiesbr <contato@indies.com.br>
2025-12-20 14:47:03 -0300

    ci: add app to docker

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "standalone",
   // Let Next.js transpile @primer packages so their CSS imports are handled correctly
   transpilePackages: ["@primer/react", "@primer/primitives"],
   compiler: {
