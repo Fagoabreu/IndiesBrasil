986a8d0c9651904535ec038712a053a12d1d7bc7
indiesbr <contato@indies.com.br>
2025-12-29 19:38:16 -0300

    ci(depoly config): deploy VPS docker

--- components/Embeds/InstagramEmbed.jsx
+++ components/Embeds/InstagramEmbed.jsx
@@ -1,23 +1,27 @@
 import { useEffect, useRef } from "react";
 import styles from "./InstagramEmbed.module.css";
+import Script from "next/script";
 
 export default function InstagramEmbed({ url }) {
   const ref = useRef(null);
 
   useEffect(() => {
-    if (!window.instgrm) {
-      const script = document.createElement("script");
-      script.src = "https://www.instagram.com/embed.js";
-      script.async = true;
-      document.body.appendChild(script);
-
-      script.onload = () => {
-        window.instgrm.Embeds.process();
-      };
-    } else {
+    if (window.instgrm?.Embeds) {
       window.instgrm.Embeds.process();
     }
   }, []);
 
-  return <blockquote ref={ref} className="instagram-media" data-instgrm-permalink={url} data-instgrm-version="14" />;
+  return (
+    <div className={styles.wrapper}>
+      {/* Script controlado pelo Next.js */}
+      <Script
+        src="https://www.instagram.com/embed.js"
+        strategy="lazyOnload"
+        onLoad={() => {
+          window.instgrm?.Embeds.process();
+        }}
+      />
+      <blockquote ref={ref} className="instagram-media" data-instgrm-permalink={url} data-instgrm-version="14" />
+    </div>
+  );
 }
