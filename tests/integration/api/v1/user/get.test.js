c4253febc5a016f83497f4cb57a7ef068b2cb93e
indiesbr <contato@indies.com.br>
2026-01-04 02:40:04 -0300

    fix: fix tests

--- tests/integration/api/v1/user/get.test.js
+++ tests/integration/api/v1/user/get.test.js
@@ -73,7 +73,7 @@ describe("GET /api/v1/user", () => {
         maxAge: session.EXPIRATION_IN_MILLISECONDS / 1000,
         path: "/",
         httpOnly: true,
-        sameSite: "Lax",
+        sameSite: "None",
       });
     });
 
@@ -200,7 +200,7 @@ describe("GET /api/v1/user", () => {
         maxAge: session.EXPIRATION_IN_MILLISECONDS / 1000,
         path: "/",
         httpOnly: true,
-        sameSite: "Lax",
+        sameSite: "None",
       });
     });
   });
