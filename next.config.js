82d926a6b042e71c42110094112a758f49f2f736
indiesbr <noisabreus@hotmail.com>
2026-05-28 22:21:01 -0300

    fix: fix twitch online embeded

--- next.config.js
+++ next.config.js
@@ -17,6 +17,11 @@ const nextConfig = {
         hostname: "res.cloudinary.com",
         pathname: "/**",
       },
+      {
+        protocol: "https",
+        hostname: "static-cdn.jtvnw.net",
+        pathname: "/**",
+      },
     ],
   },
 };
